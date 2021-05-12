f77ce2a939fb3d52c4d2746d75db09b532a61473
gnbdev <26253@qq.com>
2021-05-12 16:08:32 +0800

    支持用户自定义中继节点路径，当两个节点无法p2p通信时会尝试通过已经成功p2p通信的节点进行中继, node.conf 支持更多的配置项，日志输出控制支持更多选项

--- sh/dig_wan6.sh
+++ sh/dig_wan6.sh
@@ -2,6 +2,6 @@
 
 #dig -6 TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}' > /tmp/wan6_addr.dump
 
-wget http://api.myip.la -q -6 -O /tmp/wan6_addr.dump
+wget http://api.myip.la -t 3 -q -6 -O /tmp/wan6_addr.dump
 
 
